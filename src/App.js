3b3e3d8080102d5a32087e1e596fc89ee5dd4f01
VadimEngine <60050294+VadimEngine@users.noreply.github.com>
2026-07-08 22:14:56 -0700

    v1.1.2 updated logo

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const LS_REST_TIME    = 'abra-rest-time';
 const LS_LISTS        = 'abra-lists';
 const LS_ACTIVE_LIST  = 'abra-active-list';
 
-const APP_VERSION = '1.1.1';
+const APP_VERSION = '1.1.2';
 
 const randomId = () => Math.random().toString(36).slice(2) + Date.now().toString(36);
 
